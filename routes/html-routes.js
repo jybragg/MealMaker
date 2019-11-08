c0110e1ffbfce0285706b814633c83bc3949a352
Jasmin <jasymatos@gmail.com>
2019-11-08 18:09:03 -0500

    added search page and related routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -44,6 +44,10 @@ module.exports = function (app) {
     res.sendFile(path.join(__dirname, "../../public/index.html"));
   });
 
+  app.get("/search", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/search.html"));
+  });
+
   app.get("/signup", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/signup.html"));
   });
