78617c1e4234384def89e881558eb68cbe68e34e
Jasmin <jasymatos@gmail.com>
2019-11-15 10:21:21 -0500

    able to render all recipes from database

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-// var models = require('../models');
 var db = require("../models");
+const sequelize = require("sequelize");
+const Op = sequelize.Op;
 var passport = require("../config/passport");
-// var LocalStrategy = require("passport-local").Strategy;
+
 
 module.exports = function (app) {
 
@@ -41,4 +42,119 @@ module.exports = function (app) {
       res.redirect("/");
     });
   });
+
+//===================================================================//
+
+// Get all recipes list
+app.get("/api/search/", (req, res) => 
+  db.Post.findAll()
+  .then(posts => res.render("search", {
+    posts
+  }))
+  .catch(err => console.log(err)));
+
+
+
+
+// Route for getting all of the posts
+  // app.get("/api/search/", function (req, res) {
+  //   db.Post.findAll({})
+  //     .then(function (dbPost) {
+  //       // console.log(dbPost);
+  //       res.json(dbPost);
+  //     })
+  //     .catch(err => console.log(err));
+  // });
+
+  // Route for getting all of the posts
+  // app.get("/api/search", function (req, res) {
+  //   const { term } = req.query;
+  //   db.Post.findAll({
+  //     where: {
+  //       name: {
+  //         [Op.like]: "%" + term + "%"
+  //       }
+  //     }
+  //   })
+  //   .then(posts => res.render("members", { posts }))
+  //   .catch(err => console.log(err));
+  // });
+
+  // GET route for Searching by name
+  app.get("/api/search/:name", function (req, res) {
+    db.Post.findAll({
+      where: {
+        name: {
+          [Op.like]: "%" + req.params.name + "%"
+        }
+      }
+    })
+      .then(function (dbPost) {
+        console.log(dbPost);
+        // res.json(dbPost);
+        res.render("search", {
+          title: req.body.title,
+              body: req.body.body,
+              ingredients: req.body.Ingredients,
+              instructions: req.body.instructions
+        })
+      })
+      .catch(err => console.log(err));
+  });
+
+  
+  //POST route for saving a new post
+  // app.post("/api/search", function (req, res) {
+  //   console.log(req.body);
+  //   db.Post.create({
+  //     title: req.body.title,
+  //     body: req.body.body,
+  //     category: req.body.category
+  //   })
+  //     .then(function (dbPost) {
+  //       res.json(dbPost);
+  //     });
+  // });
+
+  // //DELETE route for deleting posts
+  // app.delete("/api/search/:id", function (req, res) {
+  //   db.Post.destroy({
+  //     where: {
+  //       id: req.params.id
+  //     }
+  //   })
+  //     .then(function (dbPost) {
+  //       res.json(dbPost);
+  //     });
+  // });
+
+  //   // PUT route for updating posts
+  //   app.put("/api/search", function (req, res) {
+  //     db.Post.update(req.body,
+  //       {
+  //         where: {
+  //           id: req.body.id
+  //         }
+  //       })
+  //       .then(function (dbPost) {
+  //         res.json(dbPost);
+  //       });
+  //   });
+
+  // //To insert into the database??
+  // app.post("/api/posts", function (req, res) {
+  //   // console.log(req.body);
+  //   db.Post.create({
+  //     title: req.body.title,
+  //     body: req.body.body,
+  //     ingredients: req.body.Ingredients,
+  //     instructions: req.body.instructions,
+  //   })
+  //     .then(function (dbPost) {
+  //       console.log(dbPost);
+  //       res.json(dbPost);
+  //     });
+  // });
+
 };
+
